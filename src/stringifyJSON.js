616bc94180cd848de1a09601cad27ca28fe0179a
Jonathan Schapiro <jschapir@gmail.com>
2015-06-05 20:34:03 -0400

    modified base case

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,29 +2,83 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+//VERSION 2
 
-var stringifyJSON = function(obj) {
-	if (obj == null){
-		alert("object is null: " + obj);
-		
-		return 'null'
-	}
-	else if (Array.isArray(obj)){
-		alert("we have an array!")
+var stringifyJSON = function(obj){
 
-	} else if (typeof obj == "object"){
-		
-		alert("We have an object: " + obj)
+	//check to see if obj = array or legit object
+	if (typeof obj !== 'object' || !Array.isArray(obj)){
+		if (typeof obj === 'string'){
+			return "\"" + obj + "\""
+		}
+		else if (obj != null && obj != undefined){
+			return obj.toString()
+		}else {
+			if (obj === null){
+				return 'null'
+			}
+			else if (obj === undefined){
+				return 'undefined'
+			}
+		}
 	}else {
-		alert("object: " + obj +typeof obj)
-	  //perform this logic on each member of obj or array
-		if (obj == null){
+		//if we have an array 
+		alert(typeof obj)
+		if (Array.isArray(obj)){
+			if (obj.length > 0){
+				var newArray = _.map(obj,function(element){
+					return stringifyJSON(element)
+				})
+				return newArray
+			}
+			else {
+				return '[]'
+			}
+			
+		}else if (typeof obj === 'object'){
+			//map values to new object using the key value pairs
+		}
+	}
 
-			return 'null'
+		
+
+}
+
+
+
+//VERSION 1
+/*var stringifyJSON = function(obj) {
+	alert("Current Object: " + obj)
+	
+	//check to see if current position is beginning of 
+	//object,array, key, or value
+	//stringify it based on certian rules for each type
+	var soloObject = false;
+	//base case
+	if (typeof obj!== 'object' && !Array.isArray(obj) || obj === null){
+		
+		//perform this logic on each member of obj or array
+		
+		if (obj == null){
+			if (soloObject === true){
+				return 'null'
+
+			}else {
+
+				return null;
+			}
+			
 
 		} else if (obj === undefined){
+			if (soloObject === true){
 
-			return 'undefined'
+				return 'undefined'
+
+			} else {
+
+				return undefined
+			}
+			
 
 		} else if (typeof obj === "string"){
 			
@@ -36,8 +90,48 @@ var stringifyJSON = function(obj) {
 			return str;
 
 		}
+		return obj;
 		
+		alert("Stringifying : " + JSON.stringify(obj))
+		return JSON.stringify(obj)
 	}
+	 if (obj!= null && typeof obj === "object" && typeof Object.keys(obj).length === "number" ){
+	 	alert("not base case: " + obj)
+	 	if (Array.isArray(obj)){
+
+	 		var stringifiedObject = [];
+
+	 		if (obj.length){
+
+		 		_.each(obj,function(element,index,list){
+		 			
+		 			if (typeof element === 'number' || typeof element === 'string'){
+		 				alert("pushing a number")
+		 				stringifiedObject.push(element)
+		 			}
+		 			else{
+		 				alert("pushing a " + typeof element)
+		 				
+						stringifiedObject.push(JSON.stringify(obj));
+		 			}
+		 		})
+
+		 		return JSON.stringify(stringifiedObject);
+
+	 		} else {
+	 			
+	 			return JSON.stringify(obj);
+	 		}
+	 	}
+	 	else if (typeof obj === 'object'){
+	 		alert('We have an object')
+	 		_.each(obj,function(value,key,list){
+
+	 		})
+	 	}
+	 }
+	 
+	
 	
   // your code goes here
   //primitive
@@ -46,4 +140,4 @@ var stringifyJSON = function(obj) {
 
   //object
 
-};
+};*/
